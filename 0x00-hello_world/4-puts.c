471d332eb9e67b8de8ff02bbcf61b18d0ee7400d
ibn-ashraf92 <abdallahtuti92@gmail.com>
2022-10-15 06:49:54 +0000

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+*main - entry point
+*Description: (put funcion with \ challange)
+*Return:0
+*
+*/
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
