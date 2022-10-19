969785be6ba73b90f3ddbeb7d144da5d4b2c6fd6
ibn-ashraf92 <abdallahtuti92@gmail.com>
2022-10-19 11:30:46 +0000

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-	for (int x = 'a'; x <= 'z'; x++)
+	for (char x = 'a'; x <= 'z'; x++)
 		{
 		putchar(x);
 		}
---
+++ 0x01-variables_if_else_while/s.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+int x = 'a';
+
+while (x <= 'z')
+	{
+	putchar(x);
+	x++;
+
+	}
+
+	return (0);
+}
