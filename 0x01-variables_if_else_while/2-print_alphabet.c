86c585a3772bcf55d3461a825cc3d8fa7d3e88b0
ibn-ashraf92 <abdallahtuti92@gmail.com>
2022-10-19 10:07:13 +0000

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main - entry point
+*Description: (right lowercaes of alhpabetic)
+*Return:0
+*
+*/
+
+int main(void)
+{
+	for (int x = 'a'; x <= 'z'; x++)
+		{
+		putchar(x);
+		}
+	return(0);
+	}
