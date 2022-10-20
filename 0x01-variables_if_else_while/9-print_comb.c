1538447da606fc79d6c7a497641ebf6855b3ce97
ibn-ashraf92 <abdallahtuti92@gmail.com>
2022-10-20 15:24:12 +0000

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+*main - entry point
+*Description: (print ascending numbers with , between)
+*Return:0
+*
+*/
+
+int main(void)
+{
+int x = '0';
+
+while (x <= '9')
+	{
+	putchar(x);
+	putchar(',');
+	putchar(' ');
+		
+	x++;
+	}
+	putchar('\n');
+
+return (0);
+}
